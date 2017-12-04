c9f6ed43e51fab96d2dffe80e02683ca1a7bd60b
Gonçalo Sousa - Direct100 <gsousa@atlasazul.pt>
2017-12-04 16:01:58 +0000

    Selectors

--- activecampaign.js
+++ activecampaign.js
@@ -15,15 +15,15 @@ if(~jQuery('#ember894 > div > button.ac_button.main.button_medium.alt2 > span.ac
 	jQuery('#ember894 > div > button.ac_button.main.button_medium.alt2 > span.action-text').text('Gerir Scoring');
 }
 
-if(~jQuery('.ac_popover-menu > ul').html().indexOf('Manage Tags')) {
-	jQuery('.ac_popover-menu > ul').html().replace('Manage Tags', 'Gerir Tags');
+if(~jQuery('.ac_popover-menu').html().indexOf('Manage Tags')) {
+	jQuery('.ac_popover-menu').html().replace('Manage Tags', 'Gerir Tags');
 }
 
 
-if(~jQuery('.ac_popover-menu > ul').html().indexOf('Manage Exclusions')) {
-	jQuery('.ac_popover-menu > ul').html().replace('Manage Exclusions', 'Gerir Exclusões');
+if(~jQuery('.ac_popover-menu').html().indexOf('Manage Exclusions')) {
+	jQuery('.ac_popover-menu').html().replace('Manage Exclusions', 'Gerir Exclusões');
 }
 
-if(~jQuery('.ac_popover-menu > ul').html().indexOf('Personal Batch Actions')) {
-	jQuery('.ac_popover-menu > ul').html().replace('Personal Batch Actions', 'Ações em lote');
+if(~jQuery('.ac_popover-menu').html().indexOf('Personal Batch Actions')) {
+	jQuery('.ac_popover-menu').html().replace('Personal Batch Actions', 'Ações em lote');
 }
