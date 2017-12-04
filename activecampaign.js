a1329d99db2ef912e0d01d82d1730a03bdb6bd16
Gonçalo Sousa - Direct100 <gsousa@atlasazul.pt>
2017-12-04 15:56:54 +0000

    Active Campaign files
    Active Campaign files added

---
+++ activecampaign.js
@@ -0,0 +1,29 @@
+//Contatos
+if(~jQuery('#contactLists > div.ac_page-title > h3').text().indexOf('All Contacts')) {
+	jQuery('#contactLists > div.ac_page-title > h3').text('Todos os Contatos');
+}
+
+if(~jQuery('#contactLists > div.empty > div.title').text().indexOf("You don't have any contacts")) {
+	jQuery('#contactLists > div.empty > div.title').text('Não tem nenhum contato');
+}
+
+if(~jQuery('#contactLists > div.empty > div.body').text().indexOf('Add or Import new contacts by clicking the "New Contact" button on the top right of the screen')) {
+	jQuery('#contactLists > div.empty > div.body').text('Adicione ou Importe novos contatos ao clicar no botão "Novo Contato" na parte superior direita do seu ecrã');
+}
+
+if(~jQuery('#ember894 > div > button.ac_button.main.button_medium.alt2 > span.action-text').text().indexOf('Manage Scoring')) {
+	jQuery('#ember894 > div > button.ac_button.main.button_medium.alt2 > span.action-text').text('Gerir Scoring');
+}
+
+if(~jQuery('.ac_popover-menu > ul').html().indexOf('Manage Tags')) {
+	jQuery('.ac_popover-menu > ul').html().replace('Manage Tags', 'Gerir Tags');
+}
+
+
+if(~jQuery('.ac_popover-menu > ul').html().indexOf('Manage Exclusions')) {
+	jQuery('.ac_popover-menu > ul').html().replace('Manage Exclusions', 'Gerir Exclusões');
+}
+
+if(~jQuery('.ac_popover-menu > ul').html().indexOf('Personal Batch Actions')) {
+	jQuery('.ac_popover-menu > ul').html().replace('Personal Batch Actions', 'Ações em lote');
+}
